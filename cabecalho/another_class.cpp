91a46a75344ca454cd4f81f0325ba2ef10ed665e
iagog <iago.macedo@esig.com.br>
2023-03-08 23:59:33 -0300

    teste com cabeçalhos para testar funcionamento

---
+++ cabecalho/another_class.cpp
@@ -0,0 +1,11 @@
+// another_class.cpp
+#include "my_class.h" // header in local directory
+#include <iostream> // header in standard library
+
+using namespace N;
+using namespace std;
+
+void my_class::do_something()
+{
+    cout << "Doing something strange mother f!" << endl;
+}
